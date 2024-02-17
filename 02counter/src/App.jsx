b714b72fe401c414681873ac9663f0f1b53681e9
roshan2345 <jha062305@gmail.com>
2024-02-17 22:33:33 +0530

    interview question

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -9,16 +9,22 @@ function App() {
  //let counter=15
  const addValue=()=>{
   // counter=counter+1;
-  if(counter<20){
-  setCounter(counter + 1);
+  // if(counter<20){
+  setCounter((prevCounter)=>prevCounter+1);
+  setCounter((prevCounter)=>prevCounter+1);
+  setCounter((prevCounter)=>prevCounter+1);
+  setCounter((prevCounter)=>prevCounter+1);
+  //ab ye hame 19 dega bcoz har baar ye call back return kar raha hai  uske prevCounter ko pass karne ke wajah se purana value store hoke deta hai.
+ 
 
-  }
+
+  // }
   // console.log(counter);
  }
  const removeValue=()=>{
-  if(counter>0){
+  // if(counter>0){
     setCounter(counter-1)
-  }
+  // }
  }
 
   return (
