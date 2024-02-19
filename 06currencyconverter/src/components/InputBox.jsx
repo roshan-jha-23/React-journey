c6d8440982c36018c0b5a7e0a31ed7be9bf261bd
roshan2345 <jha062305@gmail.com>
2024-02-19 21:55:14 +0530

    currency converter

---
+++ 06currencyconverter/src/components/InputBox.jsx
@@ -0,0 +1,59 @@
+import { React, useId } from "react";
+
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectCurrency = "usd",
+  amountDisable = false,
+  currencyDisable = false,
+  className = "",
+}) {
+  const amountInputId = useId();
+
+  return (
+    <div className={`bg-gray-100 p-3 rounded-lg text-sm flex ${className}`}>
+      <div className="w-1/2">
+        <label
+          htmlFor={amountInputId} //this is auto unique id by useID hooks
+          className="text-black font-bold mb-2 inline-block"
+        >
+          {label}
+        </label>
+        <input
+          id={amountInputId}
+          className="outline-none w-full bg-transparent py-1.5 px-3 focus:outline-none focus:border-blue-500"
+          type="number"
+          placeholder="Enter amount"
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        />
+      </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black font-bold mb-2 w-full">Currency Type</p>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          disabled={currencyDisable}
+        >
+          <option value="" disabled selected >
+            Choose currency type
+          </option>
+          {currencyOptions.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default InputBox;
